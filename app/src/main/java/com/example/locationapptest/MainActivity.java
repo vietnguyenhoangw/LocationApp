90fac0eee98e72c11e1fe5a9a520853820e77c4b
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2019-10-08 09:55:20 +0700

    :tada: First commit

---
+++ app/src/main/java/com/example/locationapptest/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.locationapptest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_LOCATION = 1000;
+    private long UPDATE_INTERVAL = 5 * 1000;  /* 5 secs */
+    private long FASTEST_INTERVAL = 3000; /* 3 sec */
+
+    Button button_getlocation;
+    TextView textView_showlocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_getlocation = findViewById(R.id.button_Getlastlocation);
+        textView_showlocation = findViewById(R.id.textView_showLocation);
+
+        button_getlocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLastLocation();
+            }
+        });
+    }
+
+    /* permission check*/
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            return true;
+        return false;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                REQUEST_LOCATION);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION && grantResults.length == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getLastLocation();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    /* get last location */
+    private void getLastLocation() {
+        if (checkPermissions())
+            requestPermissions();
+        else {
+            FusedLocationProviderClient locationClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+            locationClient.getLastLocation()
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // GPS location can be null if GPS is switched off
+                            if (location != null) {
+                                // get location but we must to close the app every next get location
+                                //textView_showlocation.setText(location.getLatitude()+ " " + location.getLongitude());
+
+                                // get location without close app,
+                                // every time click will get location at this time.
+                                startLocationUpdates();
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("MapDemoActivity", "Error trying to get last GPS location");
+                            e.printStackTrace();
+                        }
+                    });
+        }
+    }
+
+    // get location at this time.
+    protected void startLocationUpdates() {
+        // Create the location request to start receiving updates
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // 2 so mac dinh
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+
+        if (checkPermissions()) return;
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        FusedLocationProviderClient locationClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        locationClient.requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        // do work here
+                        textView_showlocation.setText(locationResult.getLastLocation().getLatitude()+ " " + locationResult.getLastLocation().getLongitude());
+                    }
+                },
+                Looper.myLooper());
+    }
+}
+
